e396777ff559bbb47a004ca52b6399056d75e027
Diego Orellana <88050715+DiegoMGE@users.noreply.github.com>
2023-02-13 18:54:14 -0600

    Update Monday.js
    Exercises 2 and 3 added.
    v1

--- Monday.js
+++ Monday.js
@@ -16,8 +16,26 @@ console.log(test.name);
 /* ==========
 * Exercise 2: "Thinkful - List and Loop Drills: Lists of lists"
 ========== */
+function process_data() {
+    return data
+        .map((number) => {
+            return number[0] - number[1];
+        })
+        .reduce((n1, n2) => {
+            return n1 * n2
+        });
+}
 
 /* ==========
 * Exercise 3: Stop gninnipS My sdroW!
 ========== */
+function spinWords(string) {
+    let word = string.split(' ');
 
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].length >= 5) {
+            word[i] = word[i].split('').reverse().join('');
+        }
+    }
+    return word.join(' ');
+}
